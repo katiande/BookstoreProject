522f76417280fededc0ef120f1c957103483d993
Andersson Kati <a1500937@myy.haaga-helia.fi>
2017-10-24 10:56:29 +0300

    Create book added

--- src/main/java/com/example/Bookstore/domain/Book.java
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -5,7 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Book {
@@ -17,7 +16,6 @@ public class Book {
 	private long year;
 	private double price;
 	
-	@JsonIgnore
 	
 	public Book(){
 		
